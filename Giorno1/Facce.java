281147d1d1e131ae0e7089af03f7ed304fdba5c8
ALBERTO HEISSL <ah2101739@adt108.adt.unipd.it>
2023-10-25 12:20:59 +0200

    Esercizi

---
+++ Giorno1/Facce.java
@@ -0,0 +1,10 @@
+public class Facce {
+	public static void main(String[] args) {
+		System.out.println(" \\\\\\|||///    ###|||###     \"\"\"\"\"\"   ");
+		System.out.println("(| O   O |)  (| O   O |)  ((~~~~~~)) ");
+		System.out.println(" (   V   )    |   8   |    ))^  ^((  ");
+		System.out.println("(  < _ >  )  (  VwwwV  )  ((  ..  )) ");
+		System.out.println(" <_______>    <_______>    )) -- ((  ");
+		System.out.println("                           ((    ))  ");
+	}
+}
---
+++ Giorno1/StampaNome.java
@@ -0,0 +1,7 @@
+public class StampaNome {
+	public static void main(String[] args) {
+		System.out.println(" ---------- ");
+		System.out.println("|   nome   |");
+		System.out.println(" ---------- ");
+	}
+}
